d8c450143a7a98e128a4d475685127025d5d1b17
ledoctah <matheusferr2015@gmail.com>
2020-11-18 18:37:31 -0300

    Added route to maintain user session

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const routes = express.Router();
 
+const crypto = require('crypto');
+
 routes.get('/', (req, res) => {
     return res.render('chat/index.njk');
+})
+
+routes.post('/name', (req, res) => {
+    const { name } = req.body;
+    
+    const hash = crypto.randomBytes(8).toString('hex');
+
+    req.session.user = {
+        name, 
+        hash
+    }
+
+    return res.redirect('/');
 })
 
 module.exports = routes;
