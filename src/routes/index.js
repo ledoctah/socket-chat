2c823b65966d52655cc3447647b547616168811d
ledoctah <matheusferr2015@gmail.com>
2020-11-19 18:19:34 -0300

    Added new routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,9 +4,10 @@ const routes = express.Router();
 const crypto = require('crypto');
 
 routes.get('/', (req, res) => {
-    return res.render('chat/index.njk');
-})
+    return res.redirect('/rooms');
+});
 
+//INITIAL
 routes.post('/name', (req, res) => {
     const { name } = req.body;
     
@@ -18,6 +19,12 @@ routes.post('/name', (req, res) => {
     }
 
     return res.redirect('/');
-})
+});
+
+//ROOMS
+const RoomController = require('../app/controllers/RoomController');
+routes.get('/rooms', RoomController.index);
+routes.get('/rooms/:hash', RoomController.join);
+routes.post('/rooms', RoomController.create);
 
 module.exports = routes;
