1ef0b4f609430839794c90525309958c8ced1fe2
ledoctah <matheusferr2015@gmail.com>
2020-11-19 18:20:16 -0300

    Added frontend socket interactions

---
+++ public/scripts/socket.js
@@ -0,0 +1,26 @@
+let me = undefined;
+
+const socket = io.connect('http://localhost:5000');
+
+const pathname_split = location.pathname.split('/');
+const hash = pathname_split[pathname_split.length-1];
+
+socket.emit('join', hash);
+
+socket.on('connected', object => {
+    if(object.status == 'connected') {
+        me = object.me;
+        
+        for(user of Object.keys(object.users)) {
+            renderUser(object.users[user]);
+        }
+    }
+});
+
+socket.on('newConnection', user => {
+    renderUser(user);
+});
+
+socket.on('disconnection', user => {
+    removeUser(user);
+});
